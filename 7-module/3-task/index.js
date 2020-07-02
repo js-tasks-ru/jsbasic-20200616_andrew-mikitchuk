011724ca83471442024e131c7583c2b4e2a06651
Andrew Mikitchuk <Andrew.Mikitchuk@gmail.com>
2020-07-02 18:06:52 +0300

    Module 7 - Task 3 - Solved

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,63 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.divSlider = document.createElement('div');
+    this.divSlider.classList.add('slider');
+
+    this.elem = this.divSlider;
+
+    this.divSlider.addEventListener('click', () => {
+      let left = event.clientX - this.elem.getBoundingClientRect().left;
+      let leftRelative = left / this.elem.offsetWidth;
+      let segments = steps - 1;
+      let approximateValue = leftRelative * segments;
+      let value = Math.round(approximateValue);
+
+      document.querySelector('.slider__value').textContent = value;
+
+      console.log(document.querySelector('.slider__steps').children);
+
+      let valuePercents = value / segments * 100;
+      
+      document.querySelector('.slider__progress').style.width = valuePercents + '%';
+      document.querySelector('.slider__thumb').style.left = valuePercents + '%';
+
+
+      let customEvent = new CustomEvent('slider-change', { // имя события должно быть именно 'slider-change'
+        detail: value, // значение 0, 1, 2, 3, 4
+        bubbles: true // событие всплывает - это понадобится в дальнейшем
+      });
+
+      this.elem.dispatchEvent(customEvent);
+
+    })
+
+
+    this.sliderThumb = document.createElement('div');
+    this.sliderThumb.classList.add('slider__thumb');
+    this.sliderThumb.style.left = '0%';
+    this.divSlider.appendChild(this.sliderThumb);
+
+
+    this.span = document.createElement('span');
+    this.span.classList.add('slider__value');
+    this.span.textContent = '0';
+    this.sliderThumb.appendChild(this.span);
+
+    this.sliderProgress = document.createElement('div');
+    this.sliderProgress.classList.add('slider__progress');
+    this.sliderProgress.style.width = '0%';
+    this.divSlider.appendChild(this.sliderProgress);
+
+    this.sliderSteps = document.createElement('div');
+    this.sliderSteps.classList.add('slider__steps');
+    this.divSlider.appendChild(this.sliderSteps);
+
+    for(let i = 0; i < steps; i++) {
+      this.span = document.createElement('span');
+      if(i==0) {
+        this.span.classList.add('slider__step-active');
+      }
+      this.sliderSteps.appendChild(this.span);
+    }
   }
 }
