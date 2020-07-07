83db304b6e11ef054710630bff510ea4ede798c1
Andrew Mikitchuk <Andrew.Mikitchuk@gmail.com>
2020-07-07 14:07:59 +0300

    Module 8 - Task 2 - Solved

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,40 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+
+    this.productGrid = document.createElement('div');
+    this.productGrid.classList.add('products-grid');
+
+    this.elem = this.productGrid;
+
+    this.productGridInner = document.createElement('div');
+    this.productGridInner.classList.add('products-grid__inner');
+    this.productGrid.appendChild(this.productGridInner);
+
+    this.createProducts(this.products);
+
+  }
+
+  createProducts(products) {
+    products.forEach((el) => {
+      let card = new ProductCard(el);
+      this.productGridInner.append(card.elem);
+    });
+  }
+
+  updateFilter(filters) {
+    this.productGridInner.innerHTML = '';
+
+    Object.assign(this.filters, filters);
+
+    this.filteredProducts = (this.products).filter(item => (this.filters.noNuts != item.nuts) || !(this.filters.noNuts))
+      .filter(item => ((this.filters.vegeterianOnly || this.filters.vegetarian) && item.vegeterian) || !(this.filters.vegeterianOnly || this.filters.vegetarian))
+      .filter(item => (this.filters.maxSpiciness >= item.spiciness) || !(this.filters.maxSpiciness))
+      .filter(item => (this.filters.category === item.category) || !(this.filters.category));
+
+    if (this.filteredProducts) {
+      this.createProducts(this.filteredProducts);
+    }
+
   }
 }
